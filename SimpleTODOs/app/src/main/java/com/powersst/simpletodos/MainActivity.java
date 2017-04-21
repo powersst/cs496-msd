8cfcce96fc0fc987f1f8c0390b02032778c19c05
Steven Powers <powersst@oregonstate.edu>
2017-04-20 17:19:44 -0700

    Simple Todos V2
    Swipe to Delete and Toasts

--- SimpleTODOs/app/src/main/java/com/powersst/simpletodos/MainActivity.java
+++ SimpleTODOs/app/src/main/java/com/powersst/simpletodos/MainActivity.java
@@ -2,17 +2,20 @@ package com.powersst.simpletodos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.ArrayDeque;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TodoAdapter.OnTodoCheckedChangeListener{
 
     //private TextView mTodoListTextView;
     private RecyclerView mTodoListRecyclerView;
@@ -22,41 +25,74 @@ public class MainActivity extends AppCompatActivity {
 
     private TodoAdapter mTodoAdapter;
 
+    private Toast mTodoToast;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState); //entry point for the application
         setContentView(R.layout.activity_main); //Sets layout to the activity_main.xml file
 
-        //mTodoListTextView = (TextView)findViewById(R.id.tv_todo_list);
         mTodoListRecyclerView = (RecyclerView)findViewById(R.id.rv_todo_list);
         mTodoEntryEditText = (EditText)findViewById(R.id.et_todo_entry_box);
-        //mTodoList = new ArrayDeque<String>();
-
         mTodoListRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         mTodoListRecyclerView.setHasFixedSize(true);
 
-        mTodoAdapter = new TodoAdapter();
+        //Item Animator
+        mTodoListRecyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        //Todo_Adapter
+        mTodoAdapter = new TodoAdapter(this);
         mTodoListRecyclerView.setAdapter(mTodoAdapter);
 
+        //Initialize Toast to Null
+        mTodoToast = null;
+
+
+        //Button Item
         Button addTodoButton = (Button)findViewById(R.id.btn_add_todo);
 
+        //Button Click
         addTodoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
                 String todoText = mTodoEntryEditText.getText().toString();
 
                 if(!TextUtils.isEmpty(todoText)) {
-                    //String currentText = mTodoListTextView.getText().toString();
-                    //mTodoList.push(todoText);
+                    mTodoListRecyclerView.scrollToPosition(0);
                     mTodoAdapter.addTodo(todoText);
-
-                    //mTodoListTextView.setText("");
-                    //for (String todo : mTodoList) {
-                        //mTodoListTextView.append(todo + "\n\n");
-                    //}
                     mTodoEntryEditText.setText("");
                 }
             }
         });
+
+
+        //Swipe to Delete (Item Touch Helper)
+        ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                ((TodoAdapter.TodoViewHolder)viewHolder).removeFromList();
+            }
+        };
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleCallback);
+        itemTouchHelper.attachToRecyclerView(mTodoListRecyclerView);
+
+
+    }
+
+    @Override
+    public void onTodoCheckedChange(String todo, boolean isChecked) {
+        if (mTodoToast != null){
+            mTodoToast.cancel();
+        }
+
+        String statusMessage = isChecked ? "COMPLETED" : "MARKED INCOMPLETE";
+        mTodoToast =  Toast.makeText(this, statusMessage + ": " + todo, Toast.LENGTH_SHORT);
+        mTodoToast.show();
     }
 }
--- SimpleTODOs/app/src/main/java/com/powersst/simpletodos/TodoAdapter.java
+++ SimpleTODOs/app/src/main/java/com/powersst/simpletodos/TodoAdapter.java
@@ -4,7 +4,10 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -14,14 +17,20 @@ import java.util.ArrayList;
 
 public class TodoAdapter extends RecyclerView.Adapter<TodoAdapter.TodoViewHolder> {
     private ArrayList<String> mTodoList;
+    private OnTodoCheckedChangeListener mCheckedChangedListener;
 
-    public TodoAdapter() {
+    public TodoAdapter(OnTodoCheckedChangeListener checkChangedListener) {
         mTodoList = new ArrayList<String>();
+        mCheckedChangedListener = checkChangedListener;
     }
 
     public void addTodo(String todo) {
         mTodoList.add(todo);
-        notifyDataSetChanged();
+        notifyItemInserted(0);
+    }
+
+    private int adapterPositionToArrayIndex(int adapterPositon){
+        return mTodoList.size() - adapterPositon - 1;
     }
 
     @Override
@@ -43,16 +52,42 @@ public class TodoAdapter extends RecyclerView.Adapter<TodoAdapter.TodoViewHolder
         holder.bind(todo);
     }
 
+
+    public interface OnTodoCheckedChangeListener {
+        void onTodoCheckedChange(String todo, boolean isChecked);
+    }
+
+    //View Holder
     class TodoViewHolder extends RecyclerView.ViewHolder {
         private TextView mTodoTextView;
 
-        public TodoViewHolder(View itemView) {
+        public TodoViewHolder(final View itemView) {
             super(itemView);
             mTodoTextView = (TextView)itemView.findViewById(R.id.tv_todo_textview);
+
+
+            CheckBox checkBox = (CheckBox)itemView.findViewById(R.id.todo_checkbox);
+            checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    String todo = mTodoList.get(adapterPositionToArrayIndex(getAdapterPosition()));
+                    mCheckedChangedListener.onTodoCheckedChange(todo, isChecked);
+                }
+            });
+        }
+
+        public void removeFromList() {
+            int position = getAdapterPosition();
+
+            mTodoList.remove(adapterPositionToArrayIndex(position));
+
+            notifyItemRemoved(position);
         }
 
         public void bind(String todo) {
             mTodoTextView.setText(todo);
         }
     }
+
+
 }
